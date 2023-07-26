3c325edcfcca38c6a21816d9b9f8a9ea5721db51
Lorenzo Lopez <lorenzo.lopez@powerschool.com>
2023-07-26 14:45:47 -0400

    getTableData Function inital

--- Report Plugin/web_root/scripts/components/pqreports/controllers/controller.js
+++ Report Plugin/web_root/scripts/components/pqreports/controllers/controller.js
@@ -4,5 +4,18 @@ define(function(require) {
     var module = require('components/pqreports/module');
     module.controller('pqReportsController', ['$scope', function($scope) {
         console.log('controller started!');
+        $scope.pqItem;
+        $scope.pqItemObj;
+        $scope.pqSelect = [
+            {title: "Report 1", path: "/ws/schema/query/com.powerschool.psu.1"},
+            {title: "Report 2", path: "/ws/schema/query/com.powerschool.psu.2"}
+        ];
+        $scope.reportTitle = 'No Report Selected';
+
+        $scope.getTableData = function() {
+            console.log('Getting the PowerQuery Data');
+            $scope.pqItemObj = JSON.parse($scope.pqItem);
+            $scope.reportTitle = $scope.pqItemObj.title;
+        };
     }]);//End Controller
 });//End define
